c76fa852db57fed36ea3790a279d66ffd9187048
VersoBot <987739+VersoBot[bot]@users.noreply.github.com>
2024-09-06 22:00:28 +0300

    Updated requirements.txt and formatted code with Black

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -17,52 +17,58 @@ def load_user(username):
     return User(username=user["name"])
 
 
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route("/login", methods=["GET", "POST"])
 def login():
     """Main login logic."""
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for("main.index"))
 
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User().get_by_username(username=login_form.username.data)
-        if user is not None and User.check_password(hashed_password=user["password"], password=login_form.password.data):
+        if user is not None and User.check_password(
+            hashed_password=user["password"], password=login_form.password.data
+        ):
             print("Password validated.")
             print(f"ID: '{user['_id']}' - Username: '{user['name']}' logging in.")
             user_obj = User(username=user["name"])
             login_user(user_obj)
-            next_page = request.args.get('next')
-            if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('main.index')
+            next_page = request.args.get("next")
+            if not next_page or url_parse(next_page).netloc != "":
+                next_page = url_for("main.index")
             return redirect(next_page)
         else:
             print(f"User '{login_form.username.data}' entered invalid credentials.")
             flash("Invalid username or password")
 
-    return render_template('login.html', title='Sign In', login_form=login_form)
+    return render_template("login.html", title="Sign In", login_form=login_form)
 
 
-@bp.route('/logout')
+@bp.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for("main.index"))
 
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route("/register", methods=["GET", "POST"])
 def register():
 
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for("main.index"))
 
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
         # Hashing the password here
         user.set_password(password=form.password.data)
         # Saving into database
         user.register()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('auth.login'))
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("auth.login"))
 
-    return render_template('register.html', title='Register', form=form)
+    return render_template("register.html", title="Register", form=form)
